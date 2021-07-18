78fa9fda9d17aabcb01dd95d65b0b4cf789c5ddf
Lrd134 <larryc3200@gmail.com>
2021-07-18 16:29:13 -0400

    commit tree c0f8f5e84aaf58ba27ea5608a7c0c0b199d6d3c0

---
+++ db/migrate/20210717165346_create_posts_tags.rb
@@ -0,0 +1,8 @@
+class CreatePostsTags < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts_tags do |t|
+      t.belongs_to :post, foreign_key: true
+      t.belongs_to :tag, foreign_key: true
+    end
+  end
+end
